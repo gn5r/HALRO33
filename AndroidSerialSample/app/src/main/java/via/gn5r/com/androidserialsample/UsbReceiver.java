8f83ca9c1adfaaf6e8b0a45c725bd7667af7be3d
gn5r <via.gn5r@gmail.com>
2017-11-13 11:14:23 +0900

    commit

---
+++ AndroidSerialSample/app/src/main/java/via/gn5r/com/androidserialsample/UsbReceiver.java
@@ -0,0 +1,18 @@
+package via.gn5r.com.androidserialsample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by gn5r on 17/11/11.
+ */
+
+public class UsbReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
